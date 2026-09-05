0541c8908301fbe53b4354af75ebedd8cfc5c655
Alexander Aung <99577843+AlexanderAung@users.noreply.github.com>
2026-09-05 15:42:08 +0630

    add guideline instructions to un/informed.py

--- informed.py
+++ informed.py
@@ -1,1 +1,6 @@
-print("informed")
+"""
+# Informed Search — informed.py
+
+1. Greedy Best-First Search
+2. A* Search
+"""
--- uninformed.py
+++ uninformed.py
@@ -1,1 +1,12 @@
-print("uninformed")
+"""
+# Task 3: Implement and Explain the Search Algorithms
+
+## Implement all six search algorithms.
+
+Uninformed Search — uninformed.py
+
+1. Breadth-First Search (BFS)
+2. Depth-First Search (DFS)
+3. Uniform-Cost Search (UCS)
+4. Iterative Deepening Search (IDS)
+"""
