0f12a7be0f4e78ead9f76a9689956d39f9978f74
Alexander Aung <99577843+AlexanderAung@users.noreply.github.com>
2026-09-06 21:43:44 +0630

    ll come back to this bec IDK what is A* search

--- informed.py
+++ informed.py
@@ -4,3 +4,11 @@
 1. Greedy Best-First Search
 2. A* Search
 """
+
+
+def gdfs(graph: dict, start: str, goal: str) -> tuple:
+    pass
+
+
+def a_star(graph: dict, start: str, goal: str) -> tuple:
+    pass
