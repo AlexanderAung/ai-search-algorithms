3053a80d64072f342b2c15b1c9cab78ebbb8cead
Alexander Aung <99577843+AlexanderAung@users.noreply.github.com>
2026-09-06 22:05:09 +0630

    flask app test, working

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("hello")
+from flask import Flask, render_template, request
+
+from uninformed import bfs
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/search", methods=["Post"])
+def search():
+    start = request.form["start"]
+    goal = request.form["goal"]
+    algo = request.form["algorightm"]
+    return "Hello from AI_Search"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
