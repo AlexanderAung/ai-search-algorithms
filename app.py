36bff84a9fe0217c9e6da36f30ffef17a23e0665
Alexander Aung <99577843+AlexanderAung@users.noreply.github.com>
2026-09-07 22:10:42 +0630

    app tested with dummy graph and is working

--- app.py
+++ app.py
@@ -5,17 +5,28 @@ from uninformed import bfs
 app = Flask(__name__)
 
 
+# Dummy graph for testing
+graph = {
+    "A": [("B", 1), ("C", 4)],
+    "B": [("A", 1), ("C", 2), ("D", 5)],
+    "C": [("A", 4), ("B", 2), ("D", 1)],
+    "D": [("B", 5), ("C", 1)],
+}
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/search", methods=["Post"])
+@app.route("/search", methods=["POST"])
 def search():
     start = request.form["start"]
     goal = request.form["goal"]
-    algo = request.form["algorightm"]
-    return "Hello from AI_Search"
+
+    path, cost = bfs(graph, start, goal)
+
+    return render_template("index.html", path=path, cost=cost)
 
 
 if __name__ == "__main__":
